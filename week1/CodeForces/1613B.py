43374ba3d2a9ecec693e5083202f4ecc758c1170
mm0o7 <mm.hooshangi10@gmail.com>
2022-02-11 23:14:13 +0330

    corrected

--- week1/CodeForces/1613B.py
+++ week1/CodeForces/1613B.py
@@ -1,21 +1,19 @@
 def absent_reminder(seq: list, len:int):
-    pairs = tuple()
+    pairs = []
     for i in range(len):
         for j in range(i+1, len):
             x = [seq[i], seq[j]]
-            pairs += tuple(x)
-            print (x)
-            pairs += tuple(x)
+            y = x[::-1]
+            pairs.append(x)
+            pairs.append(y)
     for x, y in pairs:
         if x != y and x % y not in seq:
             print(f'{x} {y}')
-def main():
+
+         
+if __name__ == "__main__":
     testcase_number = int(input())
     for i in range(testcase_number):
         a = int(input())
         input_seq = [int(x) for x in input().split(" ")]
-        absent_reminder(input_seq, a)
-        
-
-if __name__ == "__main__":
-    main()   
+        absent_reminder(input_seq, a)   
