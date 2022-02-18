d56c30489484be18d057dff55f578ba6b723b6a0
mm0o7 <mm.hooshangi10@gmail.com>
2022-02-18 23:03:42 +0330

    finall

--- jalase_3_precamp/matrix_multipy.py
+++ jalase_3_precamp/matrix_multipy.py
@@ -6,13 +6,8 @@ def multiple_matrix(matrix1: list[list], matrix2: list[list]) ->list[list]:
         for i in range(len(matrix1)):
             for j in range(len(matrix2[0])):
                 mat_ans[i][j] = reduce(lambda a, b: a+ b, map(int, map(lambda x, y: x*y, map(int, matrix1[i]), map(int, matrix2[i][j]))))
-    #print(list(map(lambda x, y: x*y, map(list, matrix1), map(list, map(list, matrix2)))))
-                print([map(lambda x, y: x*y, map(list, matrix1[i]), map(list, matrix2[j]))])
-                print(list(map(int, matrix2[j])))
-    #print(list(map(list, matrix1)))
     return mat_ans
 
 
-mat1 = [[1, 2, 3],[4, 5, 6]]
-mat2 = [[1],[2],[3]]
+
 print(multiple_matrix(mat1, mat2))
