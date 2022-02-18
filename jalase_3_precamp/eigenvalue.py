db42f13e688d6ef70ce0c11dca217d07702963f0
mm0o7 <mm.hooshangi10@gmail.com>
2022-02-18 21:21:47 +0330

    oops

--- jalase_3_precamp/eigenvalue.py
+++ jalase_3_precamp/eigenvalue.py
@@ -1,27 +1,8 @@
-def eigen_matrix(matrix: list[list], landa = None) ->list[list]:
-    mat_ans = [[0 for x in range(len(matrix))]for y in range(len(matrix))]
-    for i in range(len(matrix)):
-        for j in range(len(matrix)):
-            if i == j:
-                mat_ans[i][j] == matrix[i][j] - landa
-            else:
-                mat_ans[i][j] == matrix[i][j]
-        
-def deleted_matrix(matrix, i, j):
-    return [row[:j] + row[j + 1:] for row in (matrix[:i] + matrix[i + 1:])]
+from sympy import *
 
-def matrix_deternminant(matrix):
-    if len(matrix) == 2:
-        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+def eigen_value(matrix: list[list]) ->int:
+    m_eigenvects = matrix.eigenvects()
+    return m_eigenvects
 
-    determinant = 0
-    for i in range(len(matrix)):
-        determinant += ((-1) ** i) * matrix[0][i]* matrix_deternminant(deleted_matrix(matrix, 0, i))
-    return determinant    
+print(eigen_value())
         
-def calulate_eigenvalue(matrix: list[list]) ->int:
-    i = matrix_deternminant(eigen_matrix(matrix))
-    return i
-    
-mat = [[2, -4],[-1, -1]]
-calulate_eigenvalue(mat)    
--- jalase_3_precamp/matrix_multipy.py
+++ jalase_3_precamp/matrix_multipy.py
@@ -1,11 +1,18 @@
 from functools import reduce
 def multiple_matrix(matrix1: list[list], matrix2: list[list]) ->list[list]:
-    a = []
-    #a.append(reduce(lambda a, b: a+ b, map(lambda x, y: x*y, map(list, matrix1), map(list, map(list, matrix2)))))
+    mat_ans = []
+    if len(matrix2) == len(matrix1[0]):
+        mat_ans = [[0 for x in range(len(matrix2[0]))]for y in range(len(matrix1))]
+        for i in range(len(matrix1)):
+            for j in range(len(matrix2[0])):
+                mat_ans[i][j] = reduce(lambda a, b: a+ b, map(int, map(lambda x, y: x*y, map(int, matrix1[i]), map(int, matrix2[i][j]))))
     #print(list(map(lambda x, y: x*y, map(list, matrix1), map(list, map(list, matrix2)))))
-    print(list(map(list, map(list, matrix2))))
-    print(list(map(list, matrix1)))
-    return a
+                print([map(lambda x, y: x*y, map(list, matrix1[i]), map(list, matrix2[j]))])
+                print(list(map(int, matrix2[j])))
+    #print(list(map(list, matrix1)))
+    return mat_ans
+
+
 mat1 = [[1, 2, 3],[4, 5, 6]]
 mat2 = [[1],[2],[3]]
 print(multiple_matrix(mat1, mat2))
