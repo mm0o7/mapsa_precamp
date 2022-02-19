c3eb8ee1cd3cc39601305cf91d9f554396d9aef0
mm0o7 <mm.hooshangi10@gmail.com>
2022-02-19 20:25:53 +0330

    correct it

--- jalase_3_precamp/matrix_multipy.py
+++ jalase_3_precamp/matrix_multipy.py
@@ -5,9 +5,10 @@ def multiple_matrix(matrix1: list[list], matrix2: list[list]) ->list[list]:
         mat_ans = [[0 for x in range(len(matrix2[0]))]for y in range(len(matrix1))]
         for i in range(len(matrix1)):
             for j in range(len(matrix2[0])):
-                mat_ans[i][j] = reduce(lambda a, b: a+ b, map(int, map(lambda x, y: x*y, map(int, matrix1[i]), map(int, matrix2[i][j]))))
+                mat_ans[i][j] = reduce(lambda a, b: a+ b, map(int, map(lambda x, y: x*y, map(int, matrix1[i]), [matrix2[k][j] for k in range(len(matrix1)+1)])))
     return mat_ans
 
 
-
+mat1 = [[1,2,3],[4,5,6]]
+mat2 = [[1],[2],[3]]
 print(multiple_matrix(mat1, mat2))
